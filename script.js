7bce6558781ac2c6a4cb125345a5e905da8d2c7a
Parthiv Chatterjee <141473940+parthivchat99@users.noreply.github.com>
2024-02-08 12:12:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const yesButton = document.getElementById('yesButton');
+const noButton = document.getElementById('noButton');
+const response = document.getElementById('response');
+const buttonsContainer = document.querySelector('.options'); 
+
+let noButtonClickCount = 0;
+
+noButton.addEventListener('click', function() {
+  // Increment the click count
+  noButtonClickCount++;
+
+  // Change the response text
+  const responses = [
+    "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart"
+  ];
+  const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+  response.textContent = randomResponse;
+
+  // Enlarge the Yes button
+  yesButton.style.fontSize = (16 + noButtonClickCount * 20) + 'px';
+});
+yesButton.addEventListener('click', function() {
+    // Change the response text when Yes button is clicked
+    response.textContent = "Yay...we are couples now... promise to stay by me always";
+    buttonsContainer.style.display = 'none';
+
+    const gifContainer = document.createElement('div');
+  gifContainer.innerHTML = '<img src="milk-and-mocha.gif">';
+  document.body.appendChild(gifContainer);
+});
